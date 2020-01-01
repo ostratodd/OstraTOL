4058f37ee3527168154ca39e40008406a95ec600
Todd Oakley <oakley@ucsb.edu>
2020-01-01 13:14:19 -0800

    new analysis after adding oak13 morph

--- data/transcriptomes/subsample.sh
+++ data/transcriptomes/subsample.sh
@@ -1,6 +1,13 @@
 grep 18S oakley_2013_mbe.phytab > oakley_2013_18S.phytab
 grep 28S oakley_2013_mbe.phytab > oakley_2013_28S.phytab
 
+grep binary oakley_2013_mbe.phytab > oakley_2013_binary.phytab
+grep multi oakley_2013_mbe.phytab > oakley_2013_multi.phytab
+
 #remove gaps
 perl -pi -e 's/\-//g' oakley_2013_18S.phytab
 perl -pi -e 's/\-//g' oakley_2013_28S.phytab
+
+#change to specific dataset name
+perl -pi -e 's/\tbinary/\tbinary_Oakley_2013/g' oakley_2013_binary.phytab
+perl -pi -e 's/\tmulti/\tmultistate_Oakley_2013/g' oakley_2013_multi.phytab
