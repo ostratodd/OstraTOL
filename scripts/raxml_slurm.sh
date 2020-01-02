35e96d827c89484b229cc20990dcb41152341375
Todd Oakley <oakley@ucsb.edu>
2020-01-02 11:25:00 -0800

    adding some new files

---
+++ scripts/raxml_slurm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --nodes=1 --ntasks-per-node=20
+#SBATCH -p batch
+source /home/oakley/.bashrc
+
+#First argument is infile
+#Outfile
+today=$(date +"%m_%d_%Y")
+outfile=rax_$today
+
+
+echo "This script will now send the raxml job to slurm. Currently set up to just use GTR and not search for best model."
+echo "writing phylogeny files to $outfile"
+
+raxmlHPC -s $1 -q $2 -m BINCAT -n $outfile -p 999
+
