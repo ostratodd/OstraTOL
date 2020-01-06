4b7da85a30423797abe4ef1ae4866ad23e135303
depengl <depengli@pod-login1.podcluster>
2020-01-06 14:15:58 -0800

    change 30 to 20

--- scripts/raxml_slurm.sh
+++ scripts/raxml_slurm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --nodes=1 --ntasks-per-node=30
+#SBATCH --nodes=1 --ntasks-per-node=20
 #SBATCH -p batch
 source /home/oakley/.bashrc
 
@@ -12,4 +12,4 @@ outfile=rax_$today
 echo "This script will now send the raxml job to slurm. Currently set up to just use GTR and not search for best model."
 echo "writing phylogeny files to $outfile"
 
-raxmlHPC-PTHREADS -s $1 -q $2 -m BINCAT -n $outfile -p 999 -T 30
+raxmlHPC-PTHREADS -s $1 -q $2 -m BINCAT -n $outfile -p 999 -T 20
